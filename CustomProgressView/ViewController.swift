799c505f6a79560d3bb1b40d598467af144489bc
Shoaib Ismail <shoaib.i@emblemtechnologies.com>
2019-07-04 12:45:10 +0500

    Initial Commit

---
+++ CustomProgressView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomProgressView
+//
+//  Created by Shoaib Ismail on 04/07/2019.
+//  Copyright © 2019 Xhobi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
